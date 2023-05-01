ce47de02aa8d9bafd299a998f2b6d11ac93cbc54
@maya.skaf <maya.skaf@axtro-soft.com>
2023-05-01 21:07:54 +0300

    create repository for social media

---
+++ lib/data/repository/social_media_repo.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_axtro_soft_ecommerce/data/datasource/remote/dio/dio_client.dart';
+import 'package:flutter_axtro_soft_ecommerce/data/datasource/remote/exception/api_error_handler.dart';
+import 'package:flutter_axtro_soft_ecommerce/data/model/response/base/api_response.dart';
+import 'package:flutter_axtro_soft_ecommerce/utill/app_constants.dart';
+
+class SocialMediaRepo {
+  final DioClient dioClient;
+  SocialMediaRepo({@required this.dioClient});
+
+  Future<ApiResponse> getSocialMediaList() async {
+    try {
+      final response = await dioClient.get(
+          AppConstants.SOCIAL_MEDIA_URI);
+      return ApiResponse.withSuccess(response);
+    } catch (e) {
+      return ApiResponse.withError(ApiErrorHandler.getMessage(e));
+    }
+  }
+}
