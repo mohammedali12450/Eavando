dac4be6c11a41991c32b5fcd00cbd70e3bb7fb48
@maya.skaf <maya.skaf@axtro-soft.com>
2023-05-01 21:07:16 +0300

    create provider for social media

---
+++ lib/provider/social_media_provider.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_axtro_soft_ecommerce/data/model/response/base/api_response.dart';
+import 'package:flutter_axtro_soft_ecommerce/data/model/response/category.dart';
+import 'package:flutter_axtro_soft_ecommerce/data/model/response/social_media_model.dart';
+import 'package:flutter_axtro_soft_ecommerce/data/repository/category_repo.dart';
+import 'package:flutter_axtro_soft_ecommerce/data/repository/social_media_repo.dart';
+import 'package:flutter_axtro_soft_ecommerce/helper/api_checker.dart';
+
+class SocialMediaProvider extends ChangeNotifier {
+  final SocialMediaRepo socialMediaRepo;
+
+  SocialMediaProvider({@required this.socialMediaRepo});
+
+
+  List<SocialMedia> _socialMediaList = [];
+  int _socialMediaSelectedIndex;
+
+  List<SocialMedia> get socialMediaList => _socialMediaList;
+  int get categorySelectedIndex => _socialMediaSelectedIndex;
+
+  Future<void> getSocialMediaList(BuildContext context,bool reload) async {
+    if (_socialMediaList.length == 0 || reload) {
+      ApiResponse apiResponse = await socialMediaRepo.getSocialMediaList();
+      if (apiResponse.response != null && apiResponse.response.statusCode == 200) {
+        _socialMediaList = [];
+        apiResponse.response.data.forEach((social) {
+          _socialMediaList.add(SocialMedia.fromJson(social));
+        } );
+        _socialMediaSelectedIndex = 0;
+        notifyListeners();
+      } else {
+        ApiChecker.checkApi(context, apiResponse);
+      }
+    }
+  }
+
+  void changeSelectedIndex(int selectedIndex) {
+    _socialMediaSelectedIndex = selectedIndex;
+    notifyListeners();
+  }
+}
